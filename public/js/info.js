194bb7832c2904a52eebba3d4d4bc38c55acf6c5
Louis Rex <rlhardrock@gmail.com>
2020-12-03 21:08:54 -0500

    adicion img, estructura pg, info vue

---
+++ public/js/info.js
@@ -0,0 +1,75 @@
+const info = new Vue ({
+    el: '#info',
+    data: {
+        /* EQUIPO 1 */
+        nombre1 : 'WOLF',
+        rolscrum1 : 'Backend Developer',
+        profesion1 : 'Ingeniero Electricista',
+        deporte1: 'Trekking',
+        ciudad1: 'BMedellin',
+        GitID1 : 'rlhardrock',
+        /* EQUIPO 2 */
+        nombre2 : 'WOLVERINE',
+        rolscrum2 : 'Tester',
+        profesion2 : 'Ingeniero Informático',
+        deporte2 : 'Martial Arts',
+        ciudad2: 'Villavicencio',
+        GitID2 : 'rlhardrock',
+        /* EQUIPO 3 */
+        nombre3 : 'LUIS REY',
+        rolscrum3 : 'Database Administrator',
+        profesion3 : 'Ingeniero Zootecnista',
+        deporte3 : 'American Football SS',
+        ciudad3 : 'Bogotá',
+        GitID3 : 'rlhardrock',
+        /* EQUIPO 4 */
+        nombre4 : 'MONGOOSE',
+        rolscrum4 : 'Frontend Developer',
+        profesion4 : 'Ingeniero Agricola',
+        deporte4 : 'Lacrosse',
+        ciudad4 : 'Cali',
+        GitID4: 'rlhardrock',
+        /* EQUIPO 5 */
+        nombre5 : 'FOSSA',
+        rolscrum5 : 'Data Analytics',
+        profesion5 : 'Ingeniero Civil',
+        deporte5 : 'Crossfit',
+        ciudad5 : 'Barranquilla',
+        GitID5 : 'rlhardrock',
+    }
+})
+
+const banner = new Vue ({
+    el: '#banner',
+    data: {
+        /* PAISAJES */
+        paisaje1 : 'TUNDRA',
+        paisaje2 : 'TAIGA',
+        paisaje3 : 'JUNGLE',
+        paisaje4 : 'SAVANNA',
+        paisaje5 : 'DESERT',  
+
+        legenda1: '',
+        legenda2: '',
+        legenda3: '',
+        legenda4: '',
+        legenda5: '',
+    }
+})
+
+
+const services = new Vue ({
+            el: '#services',
+            data: {
+
+        servicio1: 'HEAVY INDUSTRY',
+        servicio2: 'FINANCIAL SYSTEM',
+        servicio3: 'AGRICULTURAL SECTOR',
+
+        detalle1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        detalle2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        detalle3: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+
+
+    }
+})  
