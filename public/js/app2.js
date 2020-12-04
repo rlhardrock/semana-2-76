f4eefe3c79616b54cc1efe24b87f7434ebd403e3
Louis Rex <rlhardrock@gmail.com>
2020-12-03 19:53:10 -0500

    adcion img team

---
+++ public/js/app2.js
@@ -0,0 +1,11 @@
+const app2 = new Vue ({
+    el: '#app2',
+    data: {
+        nombre : 'Louis Rex',
+        rolscrum : 'Database Administrator',
+        profesion : 'Ingeniero Zootecnista',
+        deporte : 'American Football SS',
+        ciudad : 'Bogotá',
+        GitID : 'rlhardrock',
+    }
+})
